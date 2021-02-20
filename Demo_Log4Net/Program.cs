f53e2ae49f044e565d6bf02290f6d16cdf5508ef
Randhall Rangel <randhall_rd@hotmail.com>
2021-02-20 20:50:55 -0300

    Begin Demo Log4Net

---
+++ Demo_Log4Net/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Demo_Log4Net
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void Main(string[] args)
+        {
+            log.Info("Bienvenidos a nuestra demo de Log4Net");
+            for (int i = 0; i < 1000; i++)
+            {
+                String rowMessage = String.Empty;
+                rowMessage = String.Format("Registro {0} - {1} - {2}", i, "PRUEBA NOMBRE", "PRUEBA APELLIDO");
+                log.Error(rowMessage);
+            }
+            Console.WriteLine("Presione enter");
+            Console.ReadLine();
+        }
+    }
+}
